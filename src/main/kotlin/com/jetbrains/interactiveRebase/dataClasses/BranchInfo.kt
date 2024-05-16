c7eab2943c94bc593956e92fa6d4c0045cd034b3
dbecerramerodi <d.becerramerodio@student.tudelft.nl>
2024-05-16 12:19:48 +0200

    rebases on top of dev

--- src/main/kotlin/com/jetbrains/interactiveRebase/dataClasses/BranchInfo.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/dataClasses/BranchInfo.kt
@@ -1,5 +1,5 @@
 package com.jetbrains.interactiveRebase.dataClasses
 
-data class BranchInfo(var name: String = "", var commits: List<CommitInfo> = mutableListOf(), val isCheckedOut: Boolean = false) {
+data class BranchInfo(var name: String = "", var commits: List<CommitInfo> = listOf(), val isCheckedOut: Boolean = false) {
     var selectedCommits: MutableList<CommitInfo> = mutableListOf()
 }
