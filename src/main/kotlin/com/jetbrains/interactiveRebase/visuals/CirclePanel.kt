fb49245e2ad62883e7659e1057ddaf2c5bfc16e8
asavova03 <A.Savova@student.tudelft.nl>
2024-05-28 18:25:44 +0200

    Changed grab pic

--- src/main/kotlin/com/jetbrains/interactiveRebase/visuals/CirclePanel.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/visuals/CirclePanel.kt
@@ -128,8 +128,6 @@ open class CirclePanel(
         val resizedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
         val g = resizedImage.createGraphics()
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC)
         g.drawImage(originalImage, 0, 0, width, height, null)
         g.dispose()
         return resizedImage
