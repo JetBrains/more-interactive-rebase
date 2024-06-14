4b9bf32800b2c94238ed5bd66da511b07a4d1dfd
Ada Turgut <a.turgut@student.tudelft.nl>
2024-06-14 10:42:45 +0200

    add condition

--- src/main/kotlin/com/jetbrains/interactiveRebase/visuals/HelpPanel.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/visuals/HelpPanel.kt
@@ -3,39 +3,43 @@ package com.jetbrains.interactiveRebase.visuals
 import com.intellij.ui.components.JBPanel
 import java.awt.BorderLayout
 import java.awt.Desktop
+import java.awt.GraphicsEnvironment
 import java.awt.event.ActionEvent
+import java.net.URI
 import javax.swing.AbstractAction
+import javax.swing.Action
+import javax.swing.JButton
 
 class HelpPanel() : JBPanel<JBPanel<*>>() {
-//    var desktop: Desktop? = if (Desktop.isDesktopSupported() && !GraphicsEnvironment.isHeadless()) Desktop.getDesktop() else null
+    var desktop: Desktop? = if (Desktop.isDesktopSupported() && !GraphicsEnvironment.isHeadless()) Desktop.getDesktop() else null
 
     /**
      * Secondary constructor for testing
      */
     constructor(desktop: Desktop) : this() {
-//        this.desktop = desktop
+        this.desktop = desktop
     }
 
     init {
         this.layout = BorderLayout()
-//        if (desktop != null) {
-//            val action: Action = MyHelpAction(desktop!!)
-//            val help = JButton(action)
-//            help.toolTipText = "Show help contents"
-//            help.putClientProperty("JButton.buttonType", "help")
-//            this.add(help, BorderLayout.SOUTH)
-//        }
+        if (desktop != null) {
+            val action: Action = MyHelpAction(desktop!!)
+            val help = JButton(action)
+            help.toolTipText = "Show help contents"
+            help.putClientProperty("JButton.buttonType", "help")
+            this.add(help, BorderLayout.SOUTH)
+        }
     }
 
     class MyHelpAction(private var desktop: Desktop) : AbstractAction() {
         override fun actionPerformed(e: ActionEvent?) {
             // TODO: Change the link to the readme accessible by everyone
-//            val uri =
-//                URI(
-//                    "https://gitlab.ewi.tudelft.nl/cse2000-software-project/2023-2024/cluster-p/12c" +
-//                        "/interactive-rebase-jetbrains/-/blob/main/README.md",
-//                )
-//            desktop.browse(uri)
+            val uri =
+                URI(
+                    "https://gitlab.ewi.tudelft.nl/cse2000-software-project/2023-2024/cluster-p/12c" +
+                        "/interactive-rebase-jetbrains/-/blob/main/README.md",
+                )
+            desktop.browse(uri)
         }
     }
 }
