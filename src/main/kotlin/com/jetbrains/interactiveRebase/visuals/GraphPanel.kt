f053ca8ecce3cc9d6e75e0296ca792055c9f35b7
Ada Turgut <a.turgut@student.tudelft.nl>
2024-06-19 17:26:17 +0200

    hopefully fix tests

--- src/main/kotlin/com/jetbrains/interactiveRebase/visuals/GraphPanel.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/visuals/GraphPanel.kt
@@ -142,6 +142,9 @@ class GraphPanel(
      * below the primary one
      */
     private fun computeVerticalOffsetOfSecondBranch(): Int {
+        if (graphInfo.addedBranch == null) {
+            return 0
+        }
         val mainCircleCount = mainBranchPanel.branchPanel.circles.size
         val addedCircleCount =
             (graphInfo.addedBranch?.currentCommits?.indexOf(graphInfo.addedBranch?.baseCommit!!) ?: 0) + 1
