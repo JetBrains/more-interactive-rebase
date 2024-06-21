8ddfa9df8fafb7080371adb31c304b23b6e39209
Galya Vergieva <G.Vergieva@student.tudelft.nl>
2024-06-21 16:02:58 +0200

    fix rebase

--- src/main/kotlin/com/jetbrains/interactiveRebase/listeners/IRRepositoryChangeListener.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/listeners/IRRepositoryChangeListener.kt
@@ -23,40 +23,6 @@ class IRRepositoryChangeListener(val project: Project) : GitRepositoryChangeList
                     project.service<ModelService>().refreshModel()
                 }
             }
-//            if(!project.service<ModelService>().noMoreCherryPicking){
-//                val leftToCherryPick = invoker.commands.filterIsInstance<CherryCommand>().size -
-//                        project.service<ModelService>().counterForCherry
-//                while(!project.service<ModelService>().isDoneCherryPicking){
-//
-//                }
-//                if(leftToCherryPick==0 && project.service<ModelService>().isDoneCherryPicking){
-//                    project.service<ModelService>().counterForCherry = 0
-//                    project.service<ModelService>().noMoreCherryPicking = true
-//                    invoker.executeCommands()
-//                }
-//            }else{
-//                if (repository.isRebaseInProgress) {
-//                    project.service<ModelService>().refreshModelDuringRebaseProcess(repository.root)
-//                } else {
-//                    project.service<ModelService>().refreshModel()
-//                }
-//            }
-
-//            val head = GitUtil.getHead(repository)
-//            if(IRGitUtils(project).isCherryPickInProcess(repository.root)){
-//                println("Cherry picking commit " + IRGitUtils(project).getCurrentCherryPickCommit(repository.root))
-//            }
-//            else if(project.service<ModelService>().branchInfo.initialCommits.map{it -> it.commit.id}.contains(head).not()
-//                    && project.service<ModelService>().cherryPickInProcess){
-            // project.service<ModelService>().cherryPickInProcess = false
-//                while(!project.service<ModelService>().isDoneCerryPicking){
-//                }
-            // invoker.executeCommands()
-//            }
-//            else {
-//                println("cherry pick is aborted")
-//                //project.service<ModelService>().fetchGraphInfo(0)
-//            }
         } else {
             if (repository.isRebaseInProgress) {
                 project.service<ModelService>().refreshModelDuringRebaseProcess(repository.root)
