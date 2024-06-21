97e757ecfb97bb205c12ee0b9a3f1f3f2d89b5c0
Galya Vergieva <G.Vergieva@student.tudelft.nl>
2024-06-21 14:20:26 +0200

    tooltip

--- src/main/kotlin/com/jetbrains/interactiveRebase/actions/gitPanel/CherryAction.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/actions/gitPanel/CherryAction.kt
@@ -12,7 +12,7 @@ import icons.DvcsImplIcons
 import javax.swing.JComponent
 
 class CherryAction :
-    DumbAwareAction("Cherry Pick", "Removes a commit from history", DvcsImplIcons.CherryPick),
+    DumbAwareAction("Cherry Pick", "Copies the selected commit and adds it to the commit history", DvcsImplIcons.CherryPick),
     CustomComponentAction {
     override fun actionPerformed(e: AnActionEvent) {
         e.project?.service<ActionService>()?.takeCherryPickAction()
