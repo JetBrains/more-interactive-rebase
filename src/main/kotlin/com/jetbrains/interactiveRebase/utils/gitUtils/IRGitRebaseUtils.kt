b71c80255208ee903e0f395391cf9367609b22d7
Marina Mădăraș <m.madara@student.tudelft.nl>
2024-06-24 11:25:34 +0200

    Fix refreshing too much

--- src/main/kotlin/com/jetbrains/interactiveRebase/utils/gitUtils/IRGitRebaseUtils.kt
+++ src/main/kotlin/com/jetbrains/interactiveRebase/utils/gitUtils/IRGitRebaseUtils.kt
@@ -56,7 +56,7 @@ class IRGitRebaseUtils(private val project: Project) {
                         GitHistoryUtils.loadDetails(project, repo?.root!!, consumer, "-n", "1")
                         var previousHead = modelService.branchInfo.initialCommits[0].commit
                         if (index != 0) {
-                            previousHead = command.commit.commit
+                            previousHead = cherryCommands[index - 1].commit.commit
                         }
                         if (previousHead == head) {
                             IRGitUtils(project).gitReset()
